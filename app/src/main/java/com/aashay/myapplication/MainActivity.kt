c0a17e7c9faf41d053efa67d513dd4388a1969bb
Aashay Pawar <41710824+aashaypawar@users.noreply.github.com>
2021-08-03 16:02:52 +0530

    Initial commit

---
+++ app/src/main/java/com/aashay/myapplication/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.aashay.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.InputFilter
+import android.text.Spanned
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Declaring and Initializing  edit text from the layout
+        val editText = findViewById<EditText>(R.id.editText)
+
+        // Assigning filters
+        editText.filters = arrayOf<InputFilter>(MinMaxFilter(1, 100))
+    }
+
+    // Custom class to define min and max for the edit text
+    inner class MinMaxFilter() : InputFilter {
+        private var intMin: Int = 0
+        private var intMax: Int = 0
+
+        // Initialized
+        constructor(minValue: Int, maxValue: Int) : this() {
+            this.intMin = minValue
+            this.intMax = maxValue
+        }
+
+        override fun filter(source: CharSequence, start: Int, end: Int, dest: Spanned, dStart: Int, dEnd: Int): CharSequence? {
+            try {
+                val input = Integer.parseInt(dest.toString() + source.toString())
+                if (isInRange(intMin, intMax, input)) {
+                    return null
+                }
+            } catch (e: NumberFormatException) {
+                e.printStackTrace()
+            }
+            return ""
+        }
+
+        // Check if input c is in between min a and max b and returns corresponding boolean
+        private fun isInRange(a: Int, b: Int, c: Int): Boolean {
+            return if (b > a) c in a..b else c in b..a
+        }
+    }
+}
